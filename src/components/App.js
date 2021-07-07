584441e29aeb214edc594cf98c577ea0ee9f2643
Saqib Hasib <saqibhasib@gmail.com>
2021-07-06 19:18:00 -0700

    VideoDetail component title and description formatted

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import SearchBar from "./SearchBar";
 import ytapi from '../apis/youtube';
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
     state = {
@@ -23,13 +24,14 @@ class App extends React.Component {
     };
 
     onVideoSelect = (video) =>{
-        console.log("from the app", video);
+        this.setState({selectedVideo: video});
     };
 
     render(){
         return (
             <div className="ui container">
                 <SearchBar onFormParentSubmit={this.onInputSubmit} />
+                <VideoDetail video={this.state.selectedVideo}/>
                 <VideoList 
                     onVideoSelection={this.onVideoSelect} 
                     videos={this.state.videos}/>
